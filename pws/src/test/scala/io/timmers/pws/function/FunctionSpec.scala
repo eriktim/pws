def5a37ae4beb37369bf271b9a33138a562ba4c0
Erik Timmers <e.timmers@gmail.com>
2021-04-27 12:07:33 +0200

    Fix date parsing

--- pws/src/test/scala/io/timmers/pws/function/FunctionSpec.scala
+++ pws/src/test/scala/io/timmers/pws/function/FunctionSpec.scala
@@ -1,9 +1,11 @@
 package io.timmers.pws.function
 
 import java.io.ByteArrayOutputStream
+import java.nio.charset.StandardCharsets
 
 import zio.ZIO
-import zio.test.Assertion.isGreaterThan
+import zio.json.{ DecoderOps, DeriveJsonDecoder, JsonDecoder }
+import zio.test.Assertion.equalTo
 import zio.test.{ DefaultRunnableSpec, ZSpec, assert }
 
 object FunctionSpec extends DefaultRunnableSpec {
@@ -11,11 +13,19 @@ object FunctionSpec extends DefaultRunnableSpec {
   def spec: ZSpec[Environment, Failure] =
     suite("Function Spec")(
       testM("should handle requests") {
+        val outputStream = new ByteArrayOutputStream()
         for {
           inputStream <- ZIO.effect(getClass.getResourceAsStream("/request.json"))
-          outputStream = new ByteArrayOutputStream()
           _            = new Function().logMeasurement(inputStream, outputStream)
-        } yield assert(outputStream.size())(isGreaterThan(0))
+          response <- ZIO.fromEither(
+                        new String(outputStream.toByteArray, StandardCharsets.UTF_8)
+                          .fromJson[HttpResponse]
+                      )
+        } yield assert(response.statusCode)(equalTo(200)) &&
+          assert(response.body)(equalTo("ACK"))
       }
     )
+
+  implicit val decoder: JsonDecoder[HttpResponse] =
+    DeriveJsonDecoder.gen[HttpResponse]
 }
